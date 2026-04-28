f7e3c5baac68ff3f025a587e43fff1395429ab94
roberto <robertoponcecl@gmail.com>
2026-04-28 14:25:51 +0200

    Creación de la clase Habilidades.py

---
+++ rpg_pro_system/models/Habilidades.py
@@ -0,0 +1,10 @@
+class Habilidades:
+    def __init__(self, nombre, descripcion, tipo, nivel_maximo, costo_mana, dano_base, id_clase):
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.tipo = tipo
+        self.nivel_maximo = nivel_maximo
+        self.costo_mana = costo_mana
+        self.dano_base = dano_base
+        self.id_clase = id_clase
+        

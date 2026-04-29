1050f1f6fa3ef7a4afeefb87dee1f0e38d7f37b5
roberto <robertoponcecl@gmail.com>
2026-04-29 14:09:13 +0200

    integración de base de datos y logs de conexión

--- rpg_pro_system/database/db.py
+++ rpg_pro_system/database/db.py
@@ -1,19 +1,34 @@
+from contextlib import contextmanager
+from email import contentmanager
+
 import psycopg2
 import os
 
-def get_connection():
-    return psycopg2.connect(
-        host=os.environ.get("DB_HOST", "db"),
-        port=os.environ.get("DB_PORT", 5432),
-        dbname=os.environ.get("DB_NAME", "rpg_db"),
-        user=os.environ.get("DB_USER", "rpguser"),
-        password=os.environ.get("DB_PASSWORD", "rpgpassword")
-    )
 
-# PROBAR CONEXIÓN: docker compose exec app python database/db.py
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://rpguser:rpgpassword@db:5432/rpg_db")
+
+@contextmanager
+def get_db_connection():
+    conn = None
+    try:
+        # Intentamos la conexión
+        conn = psycopg2.connect(DATABASE_URL)
+        print("🔗 Conexión abierta")
+        yield conn
+
+    except Exception as e:
+        print(f"❌ Error al conectar: {e}")
+        print(f"DEBUG: Intentando conectar a -> {DATABASE_URL}")
+        raise e  # Lanzamos el error para que Flask sepa que algo fue mal
+    finally:
+        if conn is not None:
+            conn.close()
+            print("🔌 Conexión cerrada automáticamente")
+
+# PROBAR CONEXIÓN:
 if __name__ == "__main__":
     try:
-        conn = get_connection()
+        conn = get_db_connection()
         print("✅ Conexión exitosa a la base de datos")
         conn.close()
     except Exception as e:
